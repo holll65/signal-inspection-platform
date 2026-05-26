38598c65b375bd4e350c5a31c6d5c9e1ed1dd4bd
hang <yang30hang@gmail.com>
2026-05-26 22:23:48 +0800

    修改说明

--- USER/config_relay.c
+++ USER/config_relay.c
@@ -5,29 +5,37 @@ void GPIO_relay_Config(void)
     GPIO_InitTypeDef GPIO_InitStructure;
 
     RCC_APB2PeriphClockCmd(
+        RCC_APB2Periph_AFIO |
         RCC_APB2Periph_GPIOA |
+        RCC_APB2Periph_GPIOB |
         RCC_APB2Periph_GPIOC |
         RCC_APB2Periph_GPIOD,
         ENABLE
     );
+
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-    // ¸ÄÕâÀï
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     GPIO_SetBits(GPIOC, GPIO_Pin_12 | GPIO_Pin_13);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    // ¸ÄÕâÀï
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     GPIO_SetBits(GPIOA, GPIO_Pin_15);
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
     GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    // ¸ÄÕâÀï
+    // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     GPIO_SetBits(GPIOD, GPIO_Pin_2);
 }
--- USER/main.c
+++ USER/main.c
@@ -1,35 +1,35 @@
   #include "stm32f10x.h"
 #include "config_relay.h"
 #include "config_delay.h"
-//说明
+//说锟斤拷
 
  int main(void)
 	 { 
-		 u16 IO_IDR;	
+		 u16 key_state;	
 	
     GPIO_relay_Config();
-     
+      
 		 while(1)
 		 { 
 			 
 				
-			 IO_IDR=(GPIOB->IDR&0xfc3b);//过滤其他IO
-				switch(IO_IDR)//模式显示
+			 key_state = GPIO_ReadInputData(GPIOB) & (GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+				switch(key_state)
 				{
 				
-				case 60475://PB12-X5状态获取
+				case (GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15):
 											   relay1 =!relay1 ;
 												 Delay_ms(200);
 				break;	
-				case 56379://PB13-X6状态获取
+				case (GPIO_Pin_12 | GPIO_Pin_14 | GPIO_Pin_15):
 											   relay2 =!relay2 ;
 												 Delay_ms(200);
 				break;	
-				case 48187://PB14-X7状态获取
+				case (GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_15):
 											   relay3 =!relay3 ;
 												 Delay_ms(200);
 				break;	
-				case 31803://PB15-X8状态获取
+				case (GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14):
 											   relay4 =!relay4 ;
 												 Delay_ms(200);
 				break;					
